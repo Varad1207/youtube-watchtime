bf55211012eadc865d26dd78872e7202e62b628b
Drishtant Ghosh <ggdrishtant@gmail.com>
2023-03-13 22:49:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 const puppeteer = require('puppeteer');
 
-const videos = [
-    'https://www.youtube.com/watch?v=VIDEO-ID-HERE', //insert your video links here, you can add as many as you want!!
-  'https://www.youtube.com/watch?v=VIDEO-ID-HERE', //insert your video links here, you can add as many as you want!!
-  'https://www.youtube.com/watch?v=VIDEO-ID-HERE' //insert your video links here, you can add as many as you want!!
-];
-
 (async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
   const page = await browser.newPage();
-  for (let i = 0; i < videos.length; i++) {
-    const video = videos[i];
-    console.log(`Playing video ${i + 1} (${video})...`);
-    await page.goto(video, { waitUntil: 'networkidle2' });
-    await page.evaluate(() => {
-      const videoPlayer = document.querySelector('video');
-      videoPlayer.autoplay = true;
-      videoPlayer.loop = true;
-    });
-    await page.waitForSelector('video:not([src])', { timeout: 60000 });
-    console.log(`Video ${i + 1} has ended.`);
-  }
-  await browser.close();
+  await page.goto('https://www.youtube.com/watch?v=VIDEO-ID-HERE&autoplay=0&loop=1') && console.log(`Started playing video...`);
 })();
